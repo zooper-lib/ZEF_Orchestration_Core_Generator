bc85570740eb55911b4cfbbfa6bf75594b3a80a6
Daniel <d.martin@web-service-kaupa.de>
2024-03-11 11:32:25 +0100

    Initial
    Created project

---
+++ example/zef_orchestration_core_generator_example.dart
@@ -0,0 +1,6 @@
+import 'package:zef_orchestration_core_generator/zef_orchestration_core_generator.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/zef_orchestration_core_generator_test.dart
@@ -0,0 +1,16 @@
+import 'package:zef_orchestration_core_generator/zef_orchestration_core_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
